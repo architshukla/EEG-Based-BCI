23b776b6cf47953898b6fb7e73cbf8f84d9f9127
Archit Shukla <architshukla2@gmail.com>
2013-11-19 21:18:50 +0530

    Added css files, started basic UI
    Signed-off-by: Archit Shukla <architshukla2@gmail.com>

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  	<title>Brain Computer Interface</title>
+  	<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<!-- Bootstrap -->
+  	<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  	<!-- Custom Styles -->
+  	<link href="assets/css/styles.css" rel="stylesheet" media="screen">
+  	<script src='assets/js/jquery.js'></script>
+  	<script src='assets/js/bootstrap.min.js'></script>
+  </head>
+  <body style="background-color:#2277bb;">
+    <?php require_once("navbar.php"); ?>
+    <div class='jumbotron'>
+      <h1>EEG based BCI <br><small><br>A Brain Computer Interface utilizing EEG signals from a neuroheadset</small></h1>
+      <button class='btn btn-danger'><span class='glyphicon glyphicon-save'></span> Download Source</button>
+      <button class='btn btn-info'><span class='glyphicon glyphicon-info-sign'></span> About</button>
+    </div>
+  </body>
+</html>
